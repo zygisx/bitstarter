2476bd4ec5483d4d0c83c978ea9177b39c6df32a
Zygimantas Gatelis <zygis.gg@gmail.com>
2013-07-08 23:04:33 +0300

    Part 1. web.js reads index.html content

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
+
+var fs = require('fs');
+
 var express = require('express');
+
+var FILE_NAME = "index.html"
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var content = fs.readFileSync(FILE_NAME);
+  response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
